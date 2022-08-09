12c87eb233ad21e0593d93af078653eb6d865b4c
Erick Reis <erickrr98@gmail.com>
2022-08-08 21:13:29 -0300

    Adicionando comentarios no código

--- src/application/calculo.java
+++ src/application/calculo.java
@@ -10,13 +10,24 @@ public class calculo {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		
+		// Valores do etanol e gasolina no valor de R$
+		
 		Combustivel C = new Combustivel(3.79, 5.29);
+		
+		
+		// Nessa instancia segue: Nome veiculo, consumo etanol urbano, consumo etanol na rodovia,
+		// consumo gasolina urbano e consumo gasolina na rodovia, e como ultimo argumento o parametro dos preços do combustivel 
+		// que estão armazenados na variavel C
 		
 		Veiculo polo = new Veiculo("Polo", 6.2, 8.1, 9.4, 12.1, C);
 		Veiculo focus = new Veiculo("Focus", 6.5, 8.7, 9.7, 12.9, C);
 		Veiculo civic = new Veiculo("Civic", 6.5, 8.6, 10.6, 13.9, C);
 		Veiculo hb20 = new Veiculo("HB20", 6.9, 8.2, 10, 11.7, C);
 
+		// Esse programa seria de utilidade public se ao invés de digitar manualmente os valores 
+		// fosse consumida uma API com os parametros de consumo do veiculo
+		
 		polo.desempenho();
 		System.out.println("---------------------");
 
